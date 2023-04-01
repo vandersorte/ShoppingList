bf7751578b054dedac984f8694491f7f8a8f04b8
Vander Sorte <101753340+vandersorte@users.noreply.github.com>
2023-04-01 19:07:43 -0300

    Add files via upload

---
+++ codigolista.py
@@ -0,0 +1,42 @@
+
+import getpass
+import os
+
+print('\n            --- LISTA DE COMPRAS ---  \n')
+carrinho = []
+
+while True:
+       
+       try:
+              escolher_acao = getpass.getpass('\n [A] anotar | [D] deletar | [V] visualizar | [S] sair \n \n ')
+
+              if escolher_acao.lower() != 'a' and escolher_acao.lower() != 'd' and escolher_acao.lower() != 'v' and escolher_acao.lower() != 's':
+                     print('Escolha umas das opções.')
+
+              elif escolher_acao.lower() == 'a':
+                     anotar = input('Anotar: ')
+                     os.system('cls')
+                     carrinho.append(anotar.lower())
+                     print(f'{anotar} Anotado.')
+                     
+              elif escolher_acao.lower() == 'd':
+                     deletar = input('Deletar: ')
+                     os.system('cls')
+                     carrinho.remove(deletar.lower())
+                     print(f'{deletar} Deletado.')
+                     
+              elif escolher_acao.lower() == 'v':
+                     os.system('cls')
+                     print('Lista completa:')
+                     for i,ordenar in enumerate(carrinho):
+                            print(i,'-', ordenar)
+                     
+              elif escolher_acao.lower() == 's':
+                     os.system('cls')
+                     print('Lista finalizada.')
+                     for i,ordenar in enumerate(carrinho):
+                            print(i,'-', ordenar)
+                     print('\n Saindo do Programa. \n','Boas Compras! \n')
+                     break 
+       except:
+              print('Erro desconhecido ou o produto descrito não está no carrinho.')
