ea76c8acaeb4dc331c530677954029a7a7ac7386
Vander Sorte <101753340+vandersorte@users.noreply.github.com>
2023-04-28 20:46:59 -0300

    Update codigolista.py

--- codigolista.py
+++ codigolista.py
@@ -8,30 +8,30 @@ listing = []
 
 while True:
        try:
-              choose_action = getpass.getpass('\n [A] anotar | [D] deletar | [V] visualizar | [S] sair \n \n ')
+              chose_action = getpass.getpass('\n [A] anotar | [D] deletar | [V] visualizar | [S] sair \n \n ')
 
-              if choose_action.lower() != 'a' and choose_action.lower() != 'd' and choose_action.lower() != 'v' and choose_action.lower() != 's':
+              if chose_action.lower() != 'a' and chose_action.lower() != 'd' and chose_action.lower() != 'v' and chose_action.lower() != 's':
                      print('Erro de digitação, escolha umas das opções.')
 
-              elif choose_action.lower() == 'a':
+              elif chose_action.lower() == 'a':
                      note = input('Anotar: ')
                      os.system('cls')
                      listing.append(note.lower())
                      print(f'{note} Anotado.')
                      
-              elif choose_action.lower() == 'd':
+              elif chose_action.lower() == 'd':
                      delete = input('Deletar: ')
                      os.system('cls')
                      listing.remove(delete.lower())
                      print(f'{delete} Deletado.')
                      
-              elif choose_action.lower() == 'v':
+              elif chose_action.lower() == 'v':
                      os.system('cls')
                      print('Lista completa:')
                      for index, order in enumerate(listing):
                             print(f'{index} - {order}')
                      
-              elif choose_action.lower() == 's':
+              elif chose_action.lower() == 's':
                      os.system('cls')
                      print('Lista finalizada.')
                      for index, order in enumerate(listing):
